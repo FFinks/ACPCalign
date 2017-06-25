781eb99132d2c774ab93c24a83d984957ebf9284
FFinks <finkel.f@husky.neu.edu>
2017-06-25 11:09:26 -0400

    X rotation
    Playing around with how to align AC and PC with 0 slope between the two. Trying out angles between z and y to rotate the volume in x. Program taking forever to run the images... Makes it challenging to visually compare volumes to determine best rotation. Will work on that in upcoming days

--- VolAlign.m
+++ VolAlign.m
@@ -5,11 +5,12 @@ pid = getDirFileNames(0,'/Users/finks/Desktop/Projects/STN_Figure/STN_Fig_Imgs')
 
 tmat=MCP.*-1; %inverse of MCP matrix for translation -necessary? 
               %check about using inverse in parameter test
-
-negangley=angley.*-1; %need to rotate opposite direction - inverse of angles
+ 
+neganglex=anglex.*-1; %need to rotate opposite direction - inverse of angles
+negangley=angley.*-1; 
 neganglez=anglez.*-1;
 
-%loop of loading patient number and cell number
+%loop of loading patient number, rotating, and saving
 for K = 1:3
 pInd=cell2mat (pid(K));   
     
@@ -20,7 +21,8 @@ mri_vol=mri.img;
 tmri_vol=imtranslate(mri_vol, tmat(K,:), 'OutputView', 'full');
 rmri_vol=imrotate3(tmri_vol,negangley(K,1),[0 1 0]);
 rbmri_vol=imrotate3(rmri_vol,neganglez(K,1),[0 0 1]);
-nii=make_nii(rbmri_vol);
+r3mri_vol=imrotate3(rbmri_vol,neganglex(K,1),[1 0 0]); %could also test order
+nii=make_nii(r3mri_vol);
 filename=sprintf('rot%s.nii',pInd);
 cd /Users/finks/Desktop/Projects/STN_Figure/STN_Fig_Rot
 save_nii(nii, filename);
